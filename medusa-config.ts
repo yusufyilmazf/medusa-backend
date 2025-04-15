8d885331c93321bb4ed8c815e0223f77961724c1
yusufyilmazf <yusufyilmaz.f@gmail.com>
2025-04-15 10:06:44 +0300

    Added admin role

--- medusa-config.ts
+++ medusa-config.ts
@@ -6,7 +6,7 @@ module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     databaseName: process.env.DATABASE_NAME ||
-      "medusa-store",
+      "defaultdb",
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
